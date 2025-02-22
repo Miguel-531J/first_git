96493ba78319d11656d514db67519649314551b0
Miguel-531J <miguel.angel.j.pelaez@gmail.com>
2025-02-21 19:47:20 -0500

    ejercicio practico modulo 1

---
+++ ejercicio_practico.py
@@ -0,0 +1,23 @@
+
+estudiante = False
+nombre = "Miguel "
+edad = 17 
+
+
+print("Nombre: ", nombre, ",tipo de variable: ", type(nombre))
+
+
+print("Edad: ", edad, ",tipo de variable: ", type(edad))
+
+
+print("Es estudiante ", estudiante, "tipo de variable:", type(estudiante))
+
+
+base = 7
+altura = 14
+perimetro = (base + altura) * 2
+print("El perimetro del rectangulo es:", perimetro)
+
+ 
+   
+
